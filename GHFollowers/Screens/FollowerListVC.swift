d21022b4b16c0f4a6ca2341ee69f6e6e12a711e3
Tprezioso <tommyprezioso@gmail.com>
2020-03-05 21:29:59 -0500

    result type refactor for results

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -17,13 +17,17 @@ class FollowerListVC: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
 
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad things happen ", message: errorMessage!.rawValue, buttonTitle: "OK")
-            return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad things happen ", message: error.rawValue, buttonTitle: "OK")
+
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
+            
         }
     }
     
