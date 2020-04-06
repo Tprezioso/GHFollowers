f6461a64591bc19a3ed7b8d61be3badbf6b7ea08
Tprezioso <tommyprezioso@gmail.com>
2020-04-06 16:38:00 -0400

    ChildVC for UserInfoVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Thomas Prezioso on 4/6/20.
+//  Copyright © 2020 Thomas Prezioso. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
