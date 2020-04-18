edaa3d7a2f406be6a433275e07021fc56600438e
Tprezioso <tommyprezioso@gmail.com>
2020-04-17 20:23:37 -0400

    added Persistence Manager

---
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,45 @@
+//
+//  PersistenceManager.swift
+//  GHFollowers
+//
+//  Created by Thomas Prezioso on 4/17/20.
+//  Copyright © 2020 Thomas Prezioso. All rights reserved.
+//
+
+import Foundation
+
+enum PersistenceManager {
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func retrieveFavorites(completed: @escaping (Result <[Follower], GFError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        
+        do {
+           let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+
+}
